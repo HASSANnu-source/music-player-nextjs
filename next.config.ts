46de55fdf480dc77a8c44161f0da73088d0286e3
HASSANnu-source <nurianyhassan@gmail.com>
2025-10-15 23:04:44 +0330

    fix show default-cover

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  output: 'export',
+  output: "export", // برای GitHub Pages ضروریه
+  basePath: isProd ? "/music-player-nextjs" : "", // اسم ریپوی توی GitHub
+  assetPrefix: isProd ? "/music-player-nextjs/" : "",
+  images: {
+    unoptimized: true, // چون GitHub Pages از Image Optimization پشتیبانی نمی‌کنه
+  },
 };
 
 export default nextConfig;
